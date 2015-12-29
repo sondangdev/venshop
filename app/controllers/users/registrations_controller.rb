2ca5f590fa7d6a54802509a1c7af4bbee93843a7
Ngoc Minh Son Dang <sondnm@zigexn.vn>
2015-12-29 14:47:13 +0700

    Add name and address to user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-# before_filter :configure_sign_up_params, only: [:create]
-# before_filter :configure_account_update_params, only: [:update]
+  before_filter :configure_sign_up_params, only: [:create]
+  before_filter :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -36,16 +36,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.for(:sign_up) << :attribute
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name, :address]
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.for(:account_update) << :attribute
+  def configure_account_update_params
+    devise_parameter_sanitizer.for(:account_update) << [:first_name, :last_name, :address]
+  end
+
+  # def update_resource(resource, params)
+  #   resource.update_without_password(params)
   # end
 
   # The path used after sign up.
