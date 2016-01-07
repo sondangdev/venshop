dfe05e6d7364e6349996bf023e07387f71394af6
Ngoc Minh Son Dang <sondnm@zigexn.vn>
2016-01-07 14:49:38 +0700

    Fix delete action

--- app/models/product.rb
+++ app/models/product.rb
@@ -33,7 +33,7 @@ class Product < ActiveRecord::Base
 
   # Make sure a product is not referenced by any line item
   def ensure_not_referenced_by_any_line_item
-    if line_item.empty?
+    if line_items.empty?
       return true
     else
       errors.add(:base, "Line items exist")
