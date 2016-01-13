a6c39c49aa4d77c1084f8660eb7bdd37c0d5158f
Ngoc Minh Son Dang <sondnm@zigexn.vn>
2016-01-13 14:01:07 +0700

    Add mailer preview to order mailer

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order_mailer
 class OrderMailerPreview < ActionMailer::Preview
-
+  def order_received
+    order = Order.first
+    OrderMailer.order_received(order)
+  end
 end
